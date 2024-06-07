fa2fe49fe61dc2911c8eaef5a2494a85f045dfbb
jason32456 <jasonchristian2112@gmail.com>
2024-06-07 22:08:52 +0700

    slider

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-let index = 0;
-
-const showItem = (index) => {
-    const carousel = document.querySelector('.carousel');
-    const items = document.querySelectorAll('.carousel-item');
-    const totalItems = items.length;
-    const itemWidth = items[0].clientWidth + 30; // item width + margin
-
-    carousel.style.transform = `translateX(${-index * itemWidth}px)`;
-};
-
-const nextItem = () => {
-    const items = document.querySelectorAll('.carousel-item');
-    index = (index + 1) % items.length;
-    showItem(index);
-};
-
-const prevItem = () => {
-    const items = document.querySelectorAll('.carousel-item');
-    index = (index - 1 + items.length) % items.length;
-    showItem(index);
-};
-
-document.querySelector('.carousel-button.next').addEventListener('click', nextItem);
-document.querySelector('.carousel-button.prev').addEventListener('click', prevItem);
-
-// Show the first item initially
-showItem(index);
-
-
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
