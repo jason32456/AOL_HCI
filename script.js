7b4b674f435f8d2deb8242003e981b08b29e8270
jason32456 <jasonchristian2112@gmail.com>
2024-06-11 17:57:14 +0700

    .

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
 //testimony slider
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-  });
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 //cart
 let cart = [];
 let total = 0;
 
 function addToCart(productName, productPrice) {
-    cart.push({ name: productName, price: productPrice });
-    total += productPrice;
-    updateCart();
+  cart.push({ name: productName, price: productPrice });
+  total += productPrice;
+  updateCart();
 }
 
 function updateCart() {
-    const cartItemsContainer = document.getElementById('cart-items');
-    cartItemsContainer.innerHTML = '';
-    cart.forEach(item => {
-        const cartItem = document.createElement('div');
-        cartItem.className = 'cart-item';
-        cartItem.innerHTML = `<span>${item.name}</span><span>$${item.price.toFixed(2)}</span>`;
-        cartItemsContainer.appendChild(cartItem);
-    });
-    document.getElementById('total').textContent = total.toFixed(2);
+  const cartItemsContainer = document.getElementById("cart-items");
+  cartItemsContainer.innerHTML = "";
+  cart.forEach((item) => {
+    const cartItem = document.createElement("div");
+    cartItem.className = "cart-item";
+    cartItem.innerHTML = `<span>${item.name}</span><span>$${item.price.toFixed(
+      2
+    )}</span>`;
+    cartItemsContainer.appendChild(cartItem);
+  });
+  document.getElementById("total").textContent = total.toFixed(2);
 }
+
+//sort
